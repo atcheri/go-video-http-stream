c569976c7170824eb6d6f99ee8788c49c759ff1d
Atsuhiro <atcheri@gmail.com>
2023-08-24 22:13:38 +0200

    feat: web page to play video

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,29 @@
-// Package main runs the ...
 package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Running the main function")
+	// configure the videos directory name and port
+	const videosDir = "videos"
+	const port = 8080
+
+	// add a handler for the song files
+	http.Handle("/", addHeaders(http.FileServer(http.Dir(videosDir))))
+	fmt.Printf("Starting server on %v\n", port)
+	log.Printf("Serving %s on HTTP port: %v\n", videosDir, port)
+
+	// serve and log errors
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+// addHeaders will act as middleware to give us CORS support
+func addHeaders(h http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		h.ServeHTTP(w, r)
+	}
 }
